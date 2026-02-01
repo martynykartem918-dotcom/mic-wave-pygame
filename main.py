3d8760bc61c4535ce424d70a3679cc74b3e82426
martynykartem918-dotcom <martynykartem918@gmail.com>
2026-02-01 19:43:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import *   
 import sounddevice as sd 
+import numpy as np
 
 # === Налаштування ===
 fs = 44100     # Частота дискретизації (кількість вимірів за секунду)
@@ -10,13 +11,14 @@ init()
 screen = display.set_mode((width, height))
 display.set_caption("Live Audio (Mic)")
 clock = time.Clock()
+font = font.SysFont("Arial", 24)
 
 # Початкові дані — масив нулів (ще немає звуку)
 data = [0.0] * chunk
 
 # === Функція, яку викликає sounddevice, коли приходить нова порція звуку ===
 def audio_callback(indata, frames, time_info, status):
-    global data
+    global audio_data, recording_data
     if status:
         print(status)  # Якщо є помилки або попередження, виводимо їх
     # Перетворюємо отриманий звук у список і масштабуємо під висоту екрану
@@ -32,13 +34,28 @@ stream = sd.InputStream(
 )
 stream.start()
 
+button_rect = Rect(200, 550, 300, 30)
+
 running = True
 while running:
+    mouse_pos = mouse.get_pos()
     for event in event.get():
         if event.type == QUIT:
             running = False
+        if event.type == MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(mouse_pos) and is_recording:
+                is_recording = False
+                stream.stop()
+                if recording_data:
+                    full_recording = concatenate(recording_data, axis = 0)
 
     screen.fill((0, 0, 0))
+    btn_color = (200, 50, 50)
+    draw.rect(screen, btn_color, button_rect, border_radius = 5)
+
+    label_text = "ЗУПИНИТИ ТА ЗБЕРЕГТИ" if is_recording else "ЗБЕРЕЖЕНО"
+    text_surf = font.render(label_text, True, (255, 255, 255))
+    screen.blit(text_surf, (button_rect.x + 10, button_rect.y +2))
 
     # Готуємо список точок для малювання хвилі
     points = []
@@ -54,5 +71,7 @@ while running:
     display.update()
     clock.tick(60)
 
-stream.stop()
 quit()
+
+
+    
